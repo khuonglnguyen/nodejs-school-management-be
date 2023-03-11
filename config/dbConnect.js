b746208d94f698b0873568cefdc1f9514e6df294
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-03-11 10:24:19 +0700

    Add connection string

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dbConnect = async () => {
   try {
-    await mongoose.connect("url");
+    await mongoose.connect("mongodb+srv://lapankhuongnguyen:6IoOsJOBDAZvI7Os@ims.q4ijqpw.mongodb.net/Ims?retryWrites=true&w=majority");
     console.log("DB connected Sunccessfully!");
   } catch (error) {
     console.log("DB connection failed!");
